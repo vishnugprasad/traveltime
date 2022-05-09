8f174cccc994571e03cc5b0e8c506d8e2c156668
vishnugprasad <vishnuanugraha001@gmail.com>
2022-05-09 05:54:56 +0530

    header

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -3,7 +3,12 @@ import "./header.css"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='mobile-menu-wrapper'>
+        <div className='flex max-width header' >
+            <img src = "https://mpng.subpng.com/20200213/yz/transparent-world-globe-flag-earth-logo-fetouh-yousef-technical-services-uae-directory-adv5e47587658e907.7518759115817340063642.jpg"></img>
+
+        </div>
+    </div>
   )
 }
 
